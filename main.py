8fb1e859e3574385b05bc3e01cca781d05c7974d
Timofii0507 <timofiiyurkov@gmail.com>
2023-10-02 14:33:59 +0300

    Перше завдання з дз

---
+++ main.py
@@ -0,0 +1,8 @@
+try:
+    print('"Anyone  who')
+    print('stops')
+    print('learning is old,')
+    print('whether at twenty or eighty"')
+    print('Henry Ford')
+except Exception as ex:
+    print(ex)
